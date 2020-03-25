52b84f2ceef61d78dbc4f2f4e62f9e557330949c
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-03-25 05:10:24 -1000

    add hoard and is_hoarded?

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,24 @@
-class Prodct
+class Product
+  attr_reader :category, :name, :unit_price, :quantity
 
-  def initialize(category, name, unit_price, quantity)
+  def initialize(category, name_parameter, unit_price, quantity)
     @category = category
     @name = name_parameter
-    @unit_price = 3.70
-    @quantity = quantity
+    @unit_price = unit_price
+    @quantity = quantity.to_i
+    @hoard = false
   end
 
   def total_price
     @unit_price * @quantity
   end
+
+  def hoard
+    @hoard = true
+  end
+
+  def is_hoarded?
+    @hoard
+  end
+
 end
