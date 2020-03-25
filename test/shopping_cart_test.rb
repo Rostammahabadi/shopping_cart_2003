e1a7a65e6a38298371ca12102b78ee2988284aac
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-03-25 05:21:15 -1000

    finish tests for initialization

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -11,4 +11,16 @@ class ShoppingCartTest < Minitest::Test
     assert_equal "King Soopers", cart.name
   end
 
+  def test_cart_has_capacity
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_equal 30, cart.capacity
+  end
+
+  def test_cart_starts_with_no_products
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_equal [], cart.products
+  end
+
 end
