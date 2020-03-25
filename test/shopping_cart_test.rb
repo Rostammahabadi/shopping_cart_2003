6451e3539bbc5b1e0075992cc3330f3d25c0b78c
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-03-25 05:17:58 -1000

    create initialize

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -5,6 +5,10 @@ require './lib/shopping_cart'
 
 class ShoppingCartTest < Minitest::Test
 
+  def test_cart_has_name
+    cart = ShoppingCart.new("King Soopers", "30items")
 
+    assert_equal "King Soopers", cart.name
+  end
 
 end
